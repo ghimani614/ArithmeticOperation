36682244282bf1b0c4798854512003243a340cf7
ghimani614 <60162829+ghimani614@users.noreply.github.com>
2021-04-01 14:44:35 -0400

    Update ArithmeticOperationTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -14,8 +14,7 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		//assertEquals(expected, actual);
-		fail("must fail");
+		assertEquals(expected, actual);
 
 	}
     @Test
